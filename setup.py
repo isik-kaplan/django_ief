94a9fbdfb4e838713cd4769fd6a3941a0222aace
Işık Kaplan <isik.kaplan@hotmail.com>
2019-08-25 17:56:53 +0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ with open('README.md') as f:
 setup(
     name='django_ief',
     version='0.1.0',
-    packages=['django_ief'],
+    packages=['django_ief', 'django_ief.templatetags'],
+    include_package_data=True,
     url='https://github.com/isik-kaplan/django_ief',
     description="A model field that can be edited without needing a form.",
     long_description=long_description,
